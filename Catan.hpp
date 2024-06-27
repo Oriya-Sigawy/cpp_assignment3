a24b0cf599c5699ed1f5f3a1461d23d258726460
Oriya-Sigawy <oriyas.07@gmail.com>
2024-06-27 16:10:23 +0300

    almost finished to write documentation to Catan.cpp. need to add test for 1 thing.

--- Catan.hpp
+++ Catan.hpp
@@ -101,9 +101,9 @@ public:
     int updateToCity(Player *p, unsigned int s);
 
     /**
-     * This function is used to buy a develop card. 
+     * This function is used to buy a develop card.
      * The function checks that p has enough resources, and if he has, takes the DC price from p's resources.
-     * @param: 
+     * @param:
      * p: the player who wants to buy a develop card.
      * dc: the type of the develop card p wants to buy.
      * @returns: -2 if there is no available DC of this type, -1 if p does not have enough resources, 0 if succeed.
@@ -116,15 +116,88 @@ public:
      * @param:
      * p: the player who wants to use the DC.
      * dc: the type of DC that p wants to use.
-     * @returns: -1 if p does not have an unused DC of dc's type, and if p has, returns the i
+     * @returns: -1 if p does not have an unused DC of dc's type, and if p has, returns the index of the DC in Catan's DCs array.
      */
     int canUseDC(Player *p, DCType dc);
+
+    /**
+     * This function is used to use DC of type Knight.
+     * The function increases p's knightCount, checks if p deserves to get the biggestArmy card, and marks the card as "used".
+     * @param:
+     *  p: the playeer who wants to use the knight.
+     * index: the index of the knight DC in Catan's DCs array.
+     * @returns: 0
+     */
     int useKnight(Player *p, Player *p2, int index);
+
+    /**
+     *This function is used to use DC of type VictoryPoint.
+     * The function increases p's victoryPoints and mark the DC as used.
+     * @param:
+     *  p: the playeer who wants to use the knight.
+     * index: the index of the victoryPoint DC in Catan's DCs array.
+     * @returns: 0
+     */
     int useVictoryPoint(Player *p, int index);
+
+    /**
+     *This function is used to use DC of type YearOfPlenty.
+     * The function gives p 1 of resource r1 and 1 of resource r2, and marks the card as "used".
+     * @param:
+     *  p: the playeer who wants to use the YearOfPlenty.
+     *  r1: the first resource p wants.
+     *  r2: the second resource p wants.
+     * index: the index of the yearOfPlenty DC in Catan's DCs array.
+     * @returns: -1 if failed, 0 if succeed.
+     */
     int useYearOfPlenty(Player *p, resourceType r1, resourceType r2, int index);
+
+    /**
+     * This function is used to use DC of type Monopoly.
+     * The function takes the resources of type rt the other players(except of p), gives them to p, and marks the card as "used".
+     * @param:
+     *  p: the playeer who wants to use the Monopoly.
+     *  rt: the resource p wants.
+     *  players: array of the players to take rt from them.
+     * index: the index of the monopoly DC in Catan's DCs array.
+     * @returns: -1 if failed, 0 if succeed.
+     */
     int useMonopoly(Player *p, resourceType rt, array<Player *, COUNT_PLAYERS> players, int index);
+
+    /**
+     * This function is used to use DC of type RoadBuilding.
+     * The function gives p resources to build 2 roads, than calls to Board's build-road with the roads p chose, and marks the card as "used".
+     * @param:
+     *  p: the playeer who wants to use the RoadBuilding.
+     *  r1: the first road p wants to build.
+     *  r2: the second road p wants to build.
+     * index: the index of the RoadBuilding DC in Catan's DCs array.
+     * @returns: the resoult of Board's build_road.
+     */
     int useRoadBuilding(Player *p, pair<unsigned int, unsigned int> r1, pair<unsigned int, unsigned int> r2, int index);
+
+    /**
+     * This function is used to trade resource between 2 players.
+     * The function split each pair of the vector to resource type and amount, takes from p2 this amount of resource in this type, and gives it to p1.
+     * @param:
+     *  resourcesToTrade: vector of pairs of resource type and amount to trade.
+     *  p1: the player that the resources will be given to.
+     *  p2: the player that the resources will be taken from.
+     */
     void trade(vector<pair<resourceType, unsigned int>> resourcesToTrade, Player *p1, Player *p2);
+
+    /**
+     * This function is used to check if p won the game.
+     * the function checks if p have 10 or more victoryPoints.
+     * @param:
+     *   p: the player to check if won.
+     * @returns: true if p has more than or equal to 10 victoryPoints, false otherwise.
+     */
     bool check_winner(Player *p);
+
+    /**
+     * This function is used to check which player has the biggestArmy card.
+     * @returns: the ID of the player that has the biggestArmy card. If there is none, the function returns -1.
+     */
     int getOwnerOfBiggestArmy();
 };
